ccb811ac08387b8382333cdeff8fd0e81c14c716
Monserrat <monserratdiaz12@gmail.com>
2018-03-17 17:47:51 +0000

    cool transition

---
+++ countdown.js
@@ -0,0 +1,24 @@
+var startTime = new Date();
+
+$(function (){
+
+function countdown() {
+
+var currentTime = startTime.getTime();
+var rightNow = new Date();
+
+
+var sec = Math.floor(( rightNow.getTime() - startTime.getTime()) / 1168);
+console.log(sec);
+
+
+sec = (sec < 10) ? "0" + sec : sec;
+
+$('.seconds').text(sec);
+
+
+setTimeout(countdown, 1000);
+}
+
+countdown();
+});
--- js/quiz.js
+++ js/quiz.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     let friend;
     let input;
     let question = document.querySelector('#QUESTION');
+    let sentences = document.querySelector('#sentences');
     
     $('#transition').hide(0);
+    $('#sentences').hide(0);
+    
     
     FADER();
     
      function FADER(){
-        $('.FADE').hide(0).delay(2000).fadeIn(2000);
+        $('.FADE').hide(0).delay(500).fadeIn(500);
         console.log('done');
      }
      
@@ -21,39 +24,43 @@ $(document).ready(function() {
          if (counter==0){
              name = input;
              counter ++;
-             $('.FADE').fadeOut(2000,function(){
+             $('.FADE').fadeOut(500,function(){
                  question.innerHTML=("<h1>Who is your favorite teacher</h1>")
                  $('input').val("");
                  
              });
              
-             $('.FADE').fadeIn(2000);
+             $('.FADE').fadeIn(500);
          }
          else if (counter ==1){
              teacher = input;
              counter ++;
-             $('.FADE').fadeOut(2000,function(){
+             $('.FADE').fadeOut(500,function(){
                  question.innerHTML=("<h1>What period do you have this teacher?</h1>")
                  $('input').val("");
              });
              
-             $('.FADE').fadeIn(2000);
+             $('.FADE').fadeIn(500);
          }
          else if (counter ==2){
              period = input;
              counter ++;
-             $('.FADE').fadeOut(2000,function(){
+             $('.FADE').fadeOut(500,function(){
                  question.innerHTML=("<h1>Who is your best friend?</h1>")
                  $('input').val("");
              });
              
-             $('.FADE').fadeIn(2000);
+             $('.FADE').fadeIn(500);
          }
          else if (counter ==3){
              friend = input;   
              counter ++;
-             $('.FADE').fadeOut(2000);
-             $('#transition').fadeIn(2000);
+             $('.FADE').fadeOut(500);
+             $('#transition').fadeIn(500);
+             $('#transition').fadeOut(500,function(){
+                Scene(); 
+             });
+             Scene();
          }
      }
      
@@ -62,6 +69,21 @@ $(document).ready(function() {
        input = $('input').val() 
        varLogic();
     });
+    
+    $('#holder').keyup(function(event){
+       if (event.keyCode === 13){
+           $('button').click();
+       } 
+    });
+    function Scene (){
+        sentences.innerHTML +=("<h1>Hi</h1>")
+    sentences.innerHTML +=("<h1>Bye</h1>")
+    $('#sentences').fadeIn(500);
+        
+    }
+    
+    
+    
         
         
         
