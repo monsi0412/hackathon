f16863e31db66f7fafa12e7b4431938dfb60ea2b
Monserrat <monserratdiaz12@gmail.com>
2018-03-17 20:15:19 +0000

    functionality

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,4 @@
+/*global $*/
 $(document).ready(function() {
     let counter = 0;
     let name;
@@ -7,100 +8,81 @@ $(document).ready(function() {
     let input;
     let question = document.querySelector('#QUESTION');
     let sentences = document.querySelector('#sentences');
-    
     let text;
 
-    
     $('#transition').hide(0);
     $('#sentences').hide(0);
     $('#page').hide(0);
-    
-    
-    
+
     FADER();
-    
-     function FADER(){
+
+    function FADER() {
         $('.FADE').hide(0).delay(500).fadeIn(500);
         console.log('done');
-     }
-     
-     function varLogic(){
-         console.log(counter);
-         if (counter==0){
-             name = input;
-             counter ++;
-             $('.FADE').fadeOut(500,function(){
-                 question.innerHTML=("<h1>Who is your favorite teacher</h1>")
-                 $('input').val("");
-                 
-             });
-             
-             $('.FADE').fadeIn(500);
-         }
-         else if (counter ==1){
-             teacher = input;
-             counter ++;
-             $('.FADE').fadeOut(500,function(){
-                 question.innerHTML=("<h1>What period do you have this teacher?</h1>")
-                 $('input').val("");
-             });
-             
-             $('.FADE').fadeIn(500);
-         }
-         else if (counter ==2){
-             period = input;
-             counter ++;
-             $('.FADE').fadeOut(500,function(){
-                 question.innerHTML=("<h1>Who is your best friend?</h1>")
-                 $('input').val("");
-             });
-             
-             $('.FADE').fadeIn(500);
-         }
-         else if (counter ==3){
-             friend = input;   
-             counter ++;
-             $('.FADE').fadeOut(500);
-             $('#transition').fadeIn(500);
-             $('#transition').fadeOut(500,function(){
-                text = "You’re in your " + period +  " period class with " + friend +  " . 'Late again, " + name + "?' " + teacher + " says as they start to hand out papers for the class. Five minutes into the period, the fire alarm suddenly goes off and a wave of silence settles over the class. While your classmates start shuffling outside, you all hear loud bangs. Gun shots. In the hall, you are met with administrators and students who are panicking, telling you to go back into the classroom. You all rush in, hiding under the desks. You see " + friend +  " a couple desks away. " + teacher +  " locks and barricades the door once everyone is inside. Silently people start pulling out their phones to contact their family in any way. You hear " + friend + " try their best to cry quietly. You need to stay calm until it’s all over."
-                Scene(); 
-             });
-             
-         }
-     }
-     
-    
-    $('button').click(function(){
-       input = $('input').val() 
-       varLogic();
+    }
+
+    function varLogic() {
+        console.log(counter);
+        if (counter == 0) {
+            name = input;
+            counter++;
+            $('.FADE').fadeOut(500, function() {
+                question.innerHTML = ("<h1>Who is your favorite teacher?</h1>")
+                $('input').val("")
+            });
+
+            $('.FADE').fadeIn(500);
+        }
+        else if (counter == 1) {
+            teacher = input;
+            counter++;
+            $('.FADE').fadeOut(500, function() {
+                question.innerHTML = ("<h1>What period do you have this teacher?</h1>")
+                $('input').val("");
+            });
+
+            $('.FADE').fadeIn(500);
+        }
+        else if (counter == 2) {
+            period = input;
+            counter++;
+            $('.FADE').fadeOut(500, function() {
+                question.innerHTML = ("<h1>Who is your best friend?</h1>")
+                $('input').val("");
+            });
+
+            $('.FADE').fadeIn(500);
+        }
+        else if (counter == 3) {
+            friend = input;
+            counter++;
+            $('.FADE').fadeOut(500);
+            $('#transition').fadeIn(2000);
+            $('#transition').fadeOut(500, function() {
+                text = "You’re in your " + period + " period class with " + friend + " . 'Late again, " + name + "?' " + teacher + " says as they start to hand out papers for the class.<br> Five minutes into the period, the fire alarm suddenly goes off and a wave of silence settles over the class. While your classmates start shuffling outside, you all hear loud bangs. Gun shots. <br> In the hall, you are met with administrators and students who are panicking, telling you to go back into the classroom. <br> You all rush in, hiding under the desks. You see " + friend + " a couple desks away. " + teacher + " locks and barricades the door once everyone is inside. <br> Silently people start pulling out their phones to contact their family in any way. You hear " + friend + " try their best to cry quietly. <br> You need to stay calm until it’s all over."
+                Scene();
+            });
+        }
+    }
+    $('button').click(function() {
+        input = $('input').val()
+        varLogic();
     });
-    $('#page').click(function(){
-       window.location.href="survey.html" 
+    $('#page').click(function() {
+        window.location.href = "survey.html"
     });
-    
-    $('#holder').keyup(function(event){
-       if (event.keyCode === 13){
-           $('#submit').click();
-       } 
+
+    $('#holder').keyup(function(event) {
+        if (event.keyCode === 13) {
+            $('#submit').click();
+        }
     });
-    function Scene (){
-        sentences.insertAdjacentHTML('beforeend',"<h3>" + text + "</h3>");
+
+    function Scene() {
+        sentences.insertAdjacentHTML('beforeend', "<h3 id='quizText'>" + text + "</h3>");
+        $('#holder').hide(0);
         $('#sentences').fadeIn(500);
         $('#page').fadeIn(500);
-        
+
     }
-    
-    
-    
-        
-        
-        
-        
-        
-    });
- 
-    
-    
-
-
+});
--- js/survey.js
+++ js/survey.js
@@ -1,1 +1,19 @@
-let choice = document.querySelector('#background1');
+$(document).ready(function() {
+
+    $('#resource').hide(0); //Learn more link 
+
+    let results = document.querySelectorAll(".result"); //Div result
+
+    function checkChoices(choices) {
+        for (let i = 0; i < choices.length; i++) {
+            results[i].innerHTML = ("<h1>" + choices[i].value + " of people said this.</h1>");
+        }
+    }
+    $('#submit').click(function() {
+        var choices = Array.from(document.querySelectorAll('.option')).filter(choice => choice.checked);
+        console.log(choices);
+        checkChoices(choices);
+        $('#submit').hide(0);
+        $('#resource').show(0);
+    });
+});
